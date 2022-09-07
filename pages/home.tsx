f5260973eb980123f18486c00aad2d4a5aa32ece
Nissim Ram <nissimram1812@gmail.com>
2022-09-07 18:21:19 -0400

    done with the left half of html and css for home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -19,7 +19,30 @@ const HomePage: NextPage = () => {
       </div>
       <div className="forecast-div">
         <button className="action-btn">Forecast</button>
-        <section className="forecast-dropdown"></section>
+        <section className="forecast-dropdown">
+          <div className="calendar-div">
+            {" "}
+            <label htmlFor="date"> Pick a date: </label>
+            <input type="date" />
+          </div>
+          <ul>
+            <li>
+              {" "}
+              <p className="forecast-result">
+                {/* add highlights for amount and date */}
+                Based on your recurring income and costs your total will be:
+                $___ on 01/01/2023
+              </p>
+            </li>
+            <li>
+              {" "}
+              <p className="forecast-result">
+                in order to stay above your threshhold you will need to make an
+                average of ___ for ___ days
+              </p>
+            </li>
+          </ul>
+        </section>
       </div>
       <section className="chart-wrapper">
         <button className="filter-time-btn"> Weekly</button>
